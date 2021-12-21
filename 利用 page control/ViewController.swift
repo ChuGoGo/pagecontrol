1ed65276c90dc402a429e6df49d683f7d838dcdb
chugogo <ioueasonae86@yahoo.com.tw>
2021-12-21 14:21:11 +0800

    Initial Commit

---
+++ 利用 page control/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  利用 page control
+//
+//  Created by Chu Go-Go on 2021/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
